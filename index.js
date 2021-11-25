ad410347173db43ed5df851ca5d90c5610c0e7af
Thomas Anderson <tanderson0876@gmail.com>
2021-11-24 22:15:37 -0500

    lots of work

---
+++ index.js
@@ -0,0 +1,23 @@
+document.getElementById("btn").addEventListener("click", function () {
+  console.log("ite");
+  document.querySelector("html").className = "new";
+  document.querySelector("#p1").textContent = "ab";
+  document.querySelector("#p2").textContent =
+    "Village: As you stroll through the barren streets of this once-lively village, a strange feeling of kenopsia washes over you. The wind carries with it a screech from the rusty hinges of the once well kept doors, piercing through the rattling of barren trees which line the outskirts of town. Watch your step as you walk, broken wood planks and shattered glass litter the cobblestone road that could have once been smooth. This town was not burned like Sodom, nor bombed like Warsaw, it suffered a fate much more sinister than the will of one man. Looking into the dirt you may miss the sight of the rusted farming tools that once tilled soil daily. The brown coating of the tools has made them frail and even the thought of touching them might make the handles snap and heads shatter. Grandeur was not thought of when this town was created, the structures could not have reached higher than the trees. The most surprising thing about this city was the lack of life from the ground. You expect ivy lining the walls and birds living in the places where there was once thatch, but none of this is present, and the area stays dead. ";
+  document.querySelector("#p3").textContent = "cd";
+  document.querySelector("#p4").textContent = "de";
+  document.querySelector("#p5").textContent = "ef";
+  document.querySelector("#p6").textContent = "fg";
+});
+
+document.getElementById("btn2").addEventListener("click", function () {
+  console.log("ite");
+  document.querySelector("html").className = "old";
+  document.querySelector("#p1").textContent = "a";
+  document.querySelector("#p2").textContent =
+    "Village:  Life flows from the open windows of the joyful cottages that line the road, and music swirls through the wind during festivals. The city center can be heard from miles away, the cheers of children and the laughter of their parents fill the surrounding grove of fruitful trees. Apple baskets lay unattended at the base of the trees, as their owners picnic in the sunlit field on clement days. As you head toward the outskirts of town the smell of mulch and fresh tilled land wafts over from the farms which support the diets of the multitude of people who live and work in the town. Life here is content with its miniature scale, seeing no reason in grand structures or fancy developments. Gardens and vines make it seem that, without tending, nature would take precedence over humanity. In a settlement where everyone contributes to the wellness of others, what will you bring with you when you visit?";
+  document.querySelector("#p3").textContent = "c";
+  document.querySelector("#p4").textContent = "d";
+  document.querySelector("#p5").textContent = "e";
+  document.querySelector("#p6").textContent = "f";
+});
